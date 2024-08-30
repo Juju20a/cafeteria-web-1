b71ca3b4e9cc62e323a18894626e75a2ff068769
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-08-30 08:53:11 -0300

    Cardapio.jsx

--- src/views/Cardapio.jsx
+++ src/views/Cardapio.jsx
@@ -20,20 +20,7 @@ const Cardapio = () => {
       <ComprasTable produtos={produtos}></ComprasTable>
       <ProdutoForm></ProdutoForm>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      
     </>
   );
 };
